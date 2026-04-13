193e41a35ce463ce3ab645e92ba0481547ebc0e4
zvolsdan <danielzvolsky350@gmail.com>
2026-04-13 17:31:02 +0200

    vault handler placeholder

--- programs/cushion/src/handlers/vault/liquidity.rs
+++ programs/cushion/src/handlers/vault/liquidity.rs
@@ -1,27 +1,60 @@
 use anchor_lang::prelude::*;
 
+// -------------------------
+// INSTRUCTION HANDLERS
+// -------------------------
 
+/// # Instruction: deposit_handler
+///
+/// Deposits tokens into the vault and provides LP tokens to the provider
+///
+/// ## Accounts:
+/// - See [`Deposit`]
+///
+/// ## Arguments
+/// - amount — amount of tokens
+///
+/// ## Errors:
+/// - `ZeroAmount`
+/// - `InsufficientFunds`
+pub fn deposit_handler(
+    ctx: Context<Deposit>,
+    amount: u64
+) -> Result<()> {
+    Ok(())
+}
+
+/// # Instruction: withdraw_handler
+///
+/// Withdraws tokens from the vault and burns providers LP tokens
+///
+/// ## Accounts:
+/// - See [`Withdraw`]
+///
+/// ## Arguments
+/// - shares — amount of tokens to withdraw
+///
+/// ## Errors:
+/// - `ZeroAmount`
+/// - `InsufficientVaultLiquidity`
+/// - `Unauthorized`
+pub fn withdraw_handler(
+    ctx: Context<Withdraw>,
+    shares: u64
+) -> Result<()> {
+    Ok(())
+}
 
 // -------------------------
 // CONTEXT STRUCTS
 // -------------------------
 
 #[derive(Accounts)]
-pub struct Deposit<'info> {
-    pub dummy: AccountInfo<'info>
+pub struct Deposit {
+
 }
 
 #[derive(Accounts)]
-pub struct MintShares<'info> {
-    pub dummy: AccountInfo<'info>
-}
+pub struct Withdraw {
 
-#[derive(Accounts)]
-pub struct Redeem<'info> {
-    pub dummy: AccountInfo<'info>
-}
-
-#[derive(Accounts)]
-pub struct Withdraw<'info> {
-    pub dummy: AccountInfo<'info>
 }
